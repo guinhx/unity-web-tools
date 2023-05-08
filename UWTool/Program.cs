c128dc08ec87f1ac3a67de67c15bf5fcd00ff5ed
Diego Santos <diegosantosaraujo@outlook.com>
2023-05-08 17:52:49 -0300

    Missing files lol

---
+++ UWTool/Program.cs
@@ -0,0 +1,45 @@
+﻿using Brotli;
+using UWTool;
+using File = System.IO.File;
+
+if (args.Length < 1)
+{
+    var helpMessage = new[]
+    {
+        "Usage:",
+        "<input_path>: Input path of .unityweb compressed file. (REQUIRED)",
+        "<output_path>: Output path to uncompressed file. (OPTIONAL)"
+    };
+    Console.WriteLine(string.Join("\n", helpMessage));
+    return;
+}
+
+
+var inputFilePath = args[0];
+var compressedData = File.ReadAllBytes(inputFilePath);
+if (UnityWeb.HasUnityMarker(compressedData))
+{
+    string outputFilePath;
+    if (args.Length < 2)
+    {
+        var directoryName = Path.GetDirectoryName(inputFilePath) ?? string.Empty;
+        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(inputFilePath).Trim();
+        var extension = Path.GetExtension(inputFilePath);
+        var outputFileName = "output_" + fileNameWithoutExtension + extension;
+        outputFilePath = Path.Combine(directoryName, outputFileName);
+    }
+    else
+    {
+        outputFilePath = args[1];
+    }
+    var fileName = Path.GetFileName(inputFilePath).Trim();
+    var uncompressedData = compressedData.DecompressFromBrotli();
+    File.WriteAllBytes(outputFilePath, uncompressedData);
+    Console.WriteLine($"{fileName} file has decompressed from Brotli.");
+    Console.WriteLine($"Output path: {outputFilePath}.");
+}
+else
+{
+    Console.WriteLine("The file don't contains brotli header.");
+}
+
---
+++ UWTool/UnityWeb.cs
@@ -0,0 +1,32 @@
+﻿namespace UWTool;
+
+public class UnityWeb
+{
+    const string BrotliHeader = "UnityWeb Compressed Content (brotli)";
+    
+    public static bool HasUnityMarker(byte[] e)
+    {
+
+        if (e.Length == 0)
+            return false;
+
+        var r = (1 & e[0]) != 0 ? (14 & e[0]) != 0 ? 4 : 7 : 1;
+        var n = e[0] & ((1 << r) - 1);
+        var o = 1 + ((int)(Math.Log(BrotliHeader.Length - 1) / Math.Log(2)) >> 3);
+        var commentOffset = (r + 1 + 2 + 1 + 2 + (o << 3) + 7) >> 3;
+
+        if (n == 17 || commentOffset > e.Length)
+            return false;
+
+        var a = n + (6 + (o << 4) + ((BrotliHeader.Length - 1) << 6) << r);
+
+        for (var i = 0; i < commentOffset; i++, a >>= 8)
+        {
+            if (e[i] != (byte)(a & 0xff))
+                return false;
+        }
+
+        var comment = System.Text.Encoding.UTF8.GetString(e, commentOffset, BrotliHeader.Length);
+        return comment == BrotliHeader;
+    }
+}
